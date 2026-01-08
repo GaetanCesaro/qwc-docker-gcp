c9a7e36f334f781f43449806ac1a452f34682617
Gaëtan CESARO <gaetan.cesaro.ext@veolia.com>
2026-01-08 14:28:16 +0100

    k8s: configmap atlas-qwc-folder-config

--- qwc-gcp/delete-qwc-k8s.sh
+++ qwc-gcp/delete-qwc-k8s.sh
@@ -1,13 +1,11 @@
 kubectl delete -f atlas-qwc-db-config-configmap.yaml
 
-kubectl delete -f qwc-admin-gui-cm1-configmap.yaml
 kubectl delete -f qwc-admin-gui-deployment.yaml
 
 kubectl delete -f qwc-api-gateway-cm0-configmap.yaml
 kubectl delete -f qwc-api-gateway-deployment.yaml
 kubectl delete -f qwc-api-gateway-service.yaml
 
-kubectl delete -f qwc-auth-service-cm1-configmap.yaml
 kubectl delete -f qwc-auth-service-deployment.yaml
 
 kubectl delete -f qwc-config-db-migrate-cm1-configmap.yaml
--- qwc-gcp/deploy-qwc-k8s.sh
+++ qwc-gcp/deploy-qwc-k8s.sh
@@ -5,14 +5,12 @@
 
 kubectl apply -f atlas-qwc-db-config-configmap.yaml -n default
 
-kubectl apply -f qwc-admin-gui-cm1-configmap.yaml -n default
 kubectl apply -f qwc-admin-gui-deployment.yaml -n default
 
 kubectl apply -f qwc-api-gateway-cm0-configmap.yaml -n default
 kubectl apply -f qwc-api-gateway-deployment.yaml -n default
 kubectl apply -f qwc-api-gateway-service.yaml -n default
 
-kubectl apply -f qwc-auth-service-cm1-configmap.yaml -n default
 kubectl apply -f qwc-auth-service-deployment.yaml -n default
 
 kubectl apply -f qwc-config-db-migrate-cm1-configmap.yaml -n default
@@ -24,11 +22,9 @@ kubectl apply -f qwc-config-service-cm4-configmap.yaml -n default
 kubectl apply -f qwc-config-service-cm5-configmap.yaml -n default
 kubectl apply -f qwc-config-service-deployment.yaml -n default
 
-kubectl apply -f qwc-data-service-cm0-configmap.yaml -n default
 kubectl apply -f qwc-data-service-cm1-configmap.yaml -n default
 kubectl apply -f qwc-data-service-deployment.yaml -n default
 
 kubectl apply -f qwc-document-service-claim2-persistentvolumeclaim.yaml -n default
 kubectl apply -f qwc-document-service-cm1-configmap.yaml -n default
-kubectl apply -f qwc-document-service-cm3-configmap.yaml -n default
 kubectl apply -f qwc-document-service-deployment.yaml -n default
